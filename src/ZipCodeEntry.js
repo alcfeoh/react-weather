87178169e05df0f4afb34186f4e67dcd56977aa6
alcfeoh <alain@interstate21.com>
2020-08-12 16:44:48 -0700

    Add zipcode validation

--- src/ZipCodeEntry.js
+++ src/ZipCodeEntry.js
@@ -2,10 +2,23 @@ import React from 'react';
 
 export class ZipCodeEntry extends React.Component {
 
-    state = { zipcode: ''};
+    state = {
+        zipcode: '',
+        error: false
+    };
 
     updateZipcode = (event) => {
-        this.setState({zipcode: event.target.value})
+        const {value} = event.target;
+        this.setState({zipcode: value, error: false});
+        if (value.length != 5 || isNaN(Number(value))) {
+            this.setState({error: true});
+        }
+    }
+
+    addZipCode = () => {
+        if (! this.state.error) {
+            this.props.onZipAdded(this.state.zipcode)
+        }
     }
 
     render() {
@@ -16,8 +29,11 @@ export class ZipCodeEntry extends React.Component {
                 <input className="form-control" placeholder="Zipcode" type="text" id="addLocation"
                       value={this.state.zipcode} onChange={this.updateZipcode} />
                 <br/>
+                {this.state.error && <div className="alert alert-danger" role="alert">
+                    Please enter a 5-digit zipcode
+                </div>}
                 <button className="btn btn-primary" id="addLocationBtn"
-                        onClick={() => onZipAdded(this.state.zipcode)}>
+                        onClick={this.addZipCode}>
                     Add location
                 </button>
             </div>
